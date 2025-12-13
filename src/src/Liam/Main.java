09b21cdac02392dfbe7f21464203f1b3015ed831
eduardo Ellwanger <dudi.cap07@gmail.com>
2025-12-13 19:24:44 -0300

    First commit: data-types, input and outputs!

---
+++ src/src/Liam/Main.java
@@ -0,0 +1,25 @@
+package Liam;
+
+public class Main {
+    public static void main(String[] args) {
+        // TWO BARS IS A SINGLE LINE COMMENT
+        /* A BAR WITH A STAR
+        * IS
+        * A
+        * MULTILINE COMMENT*/
+
+        /* VARIABLES */
+
+        // datatype var_name; Declaration
+        // var_type var_name = value_of_var; --> INITIALIZATION (declaration + value Assignment)
+
+        int brainRot; // Integers can store Positive and Negative values
+        brainRot = 67;
+
+        // CHARS and STRINGS!
+        char sex = 'y'; // a char is identified by single quotation marks ' '
+        String skibidi = "What is this diddy blud doing on the calculator?"; // While STRINGS are Identified by double quotation marks " "
+
+        System.out.println("Skibidi skibidi hawk tuah hawk");
+    }
+}
---
+++ src/src/Liam/input_Output.java
@@ -0,0 +1,32 @@
+package Liam;
+
+import java.util.Scanner; // library imported to read data inputed on the console, similar to scanf on C
+
+public class input_Output {
+    public static void input_func(String[] args) {
+
+        /* INPUTS & OUTPUTS ON JAVA */
+        /* OUTPUT */
+        System.out.println("Skibidi skibidi hawk tuah hawk");
+
+        /* INPUT */
+        Scanner scanner = new Scanner(System.in); // This is what we need to write to create a Scanner for INPUT!
+
+        int age = scanner.nextInt(); // This is going to read and save the next INT
+        System.out.println("You are " + age + " Years Old!");
+
+        // if we type anything that is not a number, we can create a scanner to capture that as well!
+        String input = scanner.next(); // next because the default value is a STRING!,
+        System.out.println("You just typed "+ input);
+
+        // Although Very simple, scanner.next() only reads the next CHAR of the input
+        // Therefore to read an entire Line of input we should use
+        /* ==      scanner.nextLine()    ==*/
+
+        /* However there is a very recurrent error when using this!
+        *
+        * sometimes, because of our code structure, the nextLine will be wasted on reading the age value so
+        * to avoid that we add another scanner.nextLine on top of the one that we want so it doesn't break the code*/
+
+    }
+}
