df0fa91ad1f41a397c603eb1daa467cb072b97d9
eduardo Ellwanger <dudi.cap07@gmail.com>
2025-12-16 12:16:50 -0300

    add: first project! Working Calculator on Main.java

--- src/src/Liam/Main.java
+++ src/src/Liam/Main.java
@@ -1,5 +1,8 @@
 package Liam;
 
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // TWO BARS IS A SINGLE LINE COMMENT
@@ -13,13 +16,92 @@ public class Main {
         // datatype var_name; Declaration
         // var_type var_name = value_of_var; --> INITIALIZATION (declaration + value Assignment)
 
-        int brainRot; // Integers can store Positive and Negative values
-        brainRot = 67;
+        //int brainRot; // Integers can store Positive and Negative values
+        //brainRot = 67;
 
         // CHARS and STRINGS!
-        char sex = 'y'; // a char is identified by single quotation marks ' '
-        String skibidi = "What is this diddy blud doing on the calculator?"; // While STRINGS are Identified by double quotation marks " "
+        //char sex = 'y'; // a char is identified by single quotation marks ' '
+        //String skibidi = "What is this diddy blud doing on the calculator?"; // While STRINGS are Identified by double quotation marks " "
 
-        System.out.println("Skibidi skibidi hawk tuah hawk");
+        //System.out.println("Skibidi skibidi hawk tuah hawk");
+
+
+        // FISRT ESSAY
+        // CREATE A CALCULATOR
+
+        /* READ THE OPERATION FIRST*/
+        // THEN READ THE TWO NUMBER
+        Scanner scanner = new Scanner(System.in);
+        int result;
+        boolean CalcRunning = true;
+        String status = "y";
+
+        System.out.println("===== WELCOME TO THE CALCULATOR =====");
+
+        while (status.equals("y")) {
+            System.out.println("Please enter the operation you will be doing (*, /, +, -): ");
+            String operation = scanner.next();
+
+            System.out.println("Please enter the First Number: ");
+            int n1 = scanner.nextInt();
+
+            System.out.println("Please enter the Second Number: ");
+            int n2 = scanner.nextInt();
+
+            switch (operation) {
+                case "+", "plus":
+                    result = n1 + n2;
+                    System.out.println("The result is" + result);
+                    System.out.println("do you wish to continue? [y][n]: ");
+                    status = scanner.next();
+                    if (status.equals("n")) {
+                        System.out.println("Closing Calculator...");
+                        break;
+                    } else {
+                        continue;
+                    }
+
+
+                case "-", "minus":
+                    result = n1 - n2;
+                    System.out.println("The result is" + result);
+                    System.out.println("do you wish to continue? [y][n]: ");
+                    status = scanner.next();
+                    if (status.equals("n")) {
+                        System.out.println("Closing Calculator...");
+                        break;
+                    } else {
+                        continue;
+                    }
+
+                case "*", "multiply":
+                    result = n1 * n2;
+                    System.out.println("The result is" + result);
+                    System.out.println("do you wish to continue? [y][n]: ");
+                    status = scanner.next();
+                    if (status.equals("n")) {
+                        System.out.println("Closing Calculator...");
+                        break;
+                    } else {
+                        continue;
+                    }
+
+                case "/", "divide":
+                    result = n1 / n2;
+                    System.out.println("The result is" + result);
+                    System.out.println("do you wish to continue? [y][n]: ");
+                    status = scanner.next();
+                    if (status.equals("n")) {
+                        System.out.println("Closing Calculator...");
+                        break;
+                    } else {
+                        continue;
+                    }
+
+                default:
+                    System.out.println("Waiting for input...");
+                    break;
+            }
+        }
     }
 }
